857d867a76f3b412d8faa596a76540f7d85a3bdf
Prerna Mishra <59911271+Prerna99-star@users.noreply.github.com>
2020-04-30 16:29:16 +0530

    k mean clustering
    one type of clustering for finding the rate of product selling

---
+++ Kmeancluster.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 01:19:51 2020
+
+K Mean clustering
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+
+# Using the Kmean method to find the optimal no. of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init ='k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Applying k-means to data set
+kmeans = KMeans(n_clusters = 5, init ='k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+#VIsualizing the clusters
+plt.scatter(X[y_kmeans ==0, 0], X[y_kmeans ==0, 1], s =100, c='red', lable= 'Cluster 1')
+plt.scatter(X[y_kmeans ==1, 0], X[y_kmeans ==0, 1], s =100, c='blue', lable= 'Cluster 2')
+plt.scatter(X[y_kmeans ==2, 0], X[y_kmeans ==0, 1], s =100, c='green', lable= 'Cluster 3')
+plt.scatter(X[y_kmeans ==3, 0], X[y_kmeans ==0, 1], s =100, c='cyan', lable= 'Cluster 4')
+plt.scatter(X[y_kmeans ==3, 0], X[y_kmeans ==0, 1], s =100, c='magenta', lable= 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s =300, c='yellow', lable= 'Centroids')
+plt.title('Cluster of Clients')
+plt.xlable('Annual Income')
+plt.ylabel('Spending score')
+plt.legend()
+plt.show()
